a9c90dcad0fb92763ae1fe2b885d21b9811255dd
cherrih <cherri.hartigan@gmail.com>
2018-12-05 16:53:35 -0800

    Create Chatterbox schema in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,14 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  roomname varchar(100),
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username varchar(100),
+  PRIMARY KEY (id)
+);
 
-
-
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  text varchar(1000),
+  createdAt DATETIME,
+  userID INT NOT NULL,
+  roomID INT NOT NULL,
+  FOREIGN KEY (userID)
+    REFERENCES users(id),
+  FOREIGN KEY (roomID) 
+    REFERENCES rooms(id),  
+  PRIMARY KEY (id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
